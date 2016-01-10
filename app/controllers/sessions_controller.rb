73f3298e435d38e31593709926ace8ba4aad9362
Richard Mable <richardbmable@gmail.com>
2016-01-10 18:55:12 -0500

    fixed strong params, user can now be created, set sessions, working on sessions actually displaying to the routes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,30 +1,42 @@
 class SessionsController < ApplicationController
 
+	
+
+	#logs a user in and sets their session
+	def create
+		#sets @user to the session params defined in the private session_params
+		@user = User.where(session_params)
+		#check that that the password matches the username provided
+		if @user && @user.password == params[:password]
+			session[:user_id] == @user.id
+			flash[:notice] = "Login was successful."
+			#sets the current user, allows use of @currentUser
+			current_user
+			#redirects to user's profile page
+			redirect_to users_index
+		else
+			#alert the user if they username and password do not match
+			flast[:alert] = "Unable to log in. Please check your username and password."
+			#reload the page so they can try again
+			redirect_to sessions_index
+		end
+	end
+
+	#logs user out
+	def destroy
+		#set the session id to nil
+		session[:user_id] == nil
+		#alert the user they have been logged out
+		flash[:notice] = "You are now logged out."
+		#redirect to the landing page
+		redirect_to home_path
+	end
+	
 	# only strong params for setting sessions
 	private
 		def session_params
 			params.require(:session).permit(:session_id)
 		end
-	end
-
-	#logs a user in and sets their session
-	def create
-		@user = User.where(sessions_params)
-		if @user && @user.password == params[:password]
-			session[:user_id] == @user.id
-			flash[:notice] = "Login was successful."
-			#redirects to user's profile page
-			redirect_to users_index
-		else
-			flast[:alert] = "Unable to log in. Please check your username and password."
-			redirect_to sessions_index
-	end
-
-	#logs user out
-	def destroy
-		session[:user_id] == nil
-		flash[:notice] = "You are now logged out."
-		redirect_to home_path
-	end
+	
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+
   def index
   	@users = User.all
   end
@@ -8,11 +9,32 @@ class UsersController < ApplicationController
   end
 
   def show
-  	@user = User.find(params[:id])
+    session[:user_id] == params[:id]
+    puts "session id"
+    puts session[:user_id]
+    current_user
+    puts "where is this"
+    puts @currentUser
+    @user = User.find(@currentUser.id)
+    
   end
 
   def create
-  	# @user = 
+  	@user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "Your account was created succesfully!"
+      session[:user_id] == @user.id
+      current_user
+      redirect_to user_path @user
+    else
+      flash[:alert] = "Your account was not created."
+      redirect_to new_user_path
+    end
   end
+
+  private
+    def user_params
+      params.require(:user).permit(:password, :username, :email)
+    end
 
 end
