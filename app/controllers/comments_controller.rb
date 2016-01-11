3b0729efcf24deecb10f42b6fe3a05d0687562b7
Richard Mable <richardbmable@gmail.com>
2016-01-11 08:48:43 -0500

    users can comment on a post, but need to add logic for displaying comments only if they exist on they post

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,6 +2,34 @@ class CommentsController < ApplicationController
   def index
   end
 
+  def new
+  	current_user
+  	@comment = Comment.new
+  end
+
+  def create
+  	current_user
+  	@comment = Comment.create(comment_params)
+  	@comment.user_id = @currentUser.id
+  	if params[:title] || params[:content] == ""
+      flash[:alert] = "Comments need to have both title and content!"
+      redirect_to posts_url
+  	elsif @comment.save
+  		flash[:notice] = "Your comment was posted."
+  		redirect_to posts_url
+  	else
+  		flash[:alert] = "There was a problem creating your commment."
+  		redirect_to posts_url
+  	end
+  end
+
+
   def show
+  end
+
+  private
+
+  def comment_params
+    params.require(:commment).permit(:user_id, :post_id, :title, :content)
   end
 end
--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -4,6 +4,7 @@ class ProfilesController < ApplicationController
 
   def new
   	current_user
+    @profile = Profile.new
   end
 
 
@@ -14,12 +15,16 @@ class ProfilesController < ApplicationController
   	@profile = Profile.create(profile_params)
   	#update the profile's user_id to the current user's id
   	@profile.user_id = @currentUser.id
+  	#would like to have a way to check if they've already created a profile
+  	# if @profile.exists?(@currentUser.id)
+  	#   flash[:alert] = "You've already created a profile! Try the update profile page."
+   #    redirect_to profiles_url
   	if @profile.save
       flash[:notice] = "Your profile was created successfully."
       redirect_to profiles_url
      else
       flash[:alert] = "There was a problem creating your profile."
-      redirect_to profiles_new_url
+      redirect_to new_profile_url
      end
   end
 
