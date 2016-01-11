70c67250115e2a26a0711a7b08536e20959888ec
Richard Mable <richardbmable@gmail.com>
2016-01-11 07:45:25 -0500

    profiles can now be created by the user, and get assigned the current user's id

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,13 +3,17 @@ class ProfilesController < ApplicationController
   end
 
   def new
+  	current_user
   end
 
 
   def create
-  	current_user
-  	:profile[user_id] = @currentUser.id
+  	#set the current user
+  	current_user	
+  	#create the new profile with the provided user input from the form
   	@profile = Profile.create(profile_params)
+  	#update the profile's user_id to the current user's id
+  	@profile.user_id = @currentUser.id
   	if @profile.save
       flash[:notice] = "Your profile was created successfully."
       redirect_to profiles_url
